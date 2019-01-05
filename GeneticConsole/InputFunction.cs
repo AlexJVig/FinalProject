c7fabe43031614d60786299e55eee97a8ba3dd9e
Unknown <ofir36@gmail.com>
2019-01-05 18:33:27 +0200

    Work on basic GA console app

---
+++ GeneticConsole/InputFunction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneticConsole
+{
+    public class InputFunction
+    {
+        public int[] Parameters { get; set; }
+        public int Result { get; set; }
+        public int Length { get { return Parameters.Length; } }
+
+        public InputFunction(int result, params int[] parameters)
+        {
+            Parameters = parameters;
+            Result = result;
+        }
+    }
+}
